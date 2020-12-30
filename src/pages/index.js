422654be8544977a56136939064f9ab40036186a
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-12-30 18:30:32 -0500

    [auth] WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,24 @@
-import React from "react"
+// TODO: https://docs.amplify.aws/lib/auth/social/q/platform/js#setup-frontend
 
+import React from "react"
+import Amplify, { Auth } from 'aws-amplify';
+import { AmplifySignIn, AmplifySignOut } from "@aws-amplify/ui-react";
+
+
+import awsconfig from '../aws-exports';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
 import Products from "../components/Products/Products"
+
+Amplify.configure(awsconfig);
 
 const AdvancedExamplePage = () => (
   <Layout>
-    <SEO title="Shop" />
-    <h1>This is the advanced example</h1>
-    <Products />
+      <AmplifySignIn federated={{ description: 'yo', type: 'facebook' }} />
+      <SEO title="Shop" />
+      <h1>This is the advanced example</h1>
+      <Products />
   </Layout>
 )
 
-export default AdvancedExamplePage
+export default AdvancedExamplePage;
---
+++ src/utils/auth.js
@@ -0,0 +1,61 @@
+// manual sign in process https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js#sign-up
+
+import { Auth } from 'aws-amplify';
+
+// Manual Sign Up Process:
+const signUpDetails = {
+    username,
+    password,
+    attributes: {
+        email,          // optional
+        phone_number,   // optional - E.164 number convention
+        // other custom attributes 
+    }
+}
+
+export const signUp = async (details = signUpDetails) => {
+    try {
+        const { user } = await Auth.signUp(details);
+        console.log(user);
+    } catch (error) {
+        console.log('error signing up:', error);
+    }
+}
+
+export const confirmSignUp = async (username, code) => {
+    try {
+      await Auth.confirmSignUp(username, code);
+    } catch (error) {
+        console.log('error confirming sign up', error);
+    }
+}
+
+export const signIn = async () => {
+    try {
+        const user = await Auth.signIn(username, password);
+    } catch (error) {
+        console.log('error signing in', error);
+    }
+}
+
+export const resendConfirmationCode = async () => {
+    try {
+        await Auth.resendSignUp(username);
+        console.log('code resent successfully');
+    } catch (err) {
+        console.log('error resending code: ', err);
+    }
+}
+
+export const signOut = async (isGlobal = false) => {
+    try {
+        if (isGlobal) {
+            await Auth.signOut({ global: true });
+        }
+        else {
+            await Auth.signOut();
+        }
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+}
